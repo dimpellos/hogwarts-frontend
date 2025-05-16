fc7adc96c6a6de715e8c5e4763042cc58055e520
Dimitris Bellos <dimpellos@gmail.com>
2025-05-16 12:09:56 +0300

    have visibility from deployed backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000';
-
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
 export const fetchHousesByQuery = async (query: string) => {
-  const response = await axios.get(`${API_BASE_URL}/houses/search?query=${query}`);
+  const response = await axios.get(`${API_BASE_URL}/houses?name=${query}`);
   return response.data;
 };
